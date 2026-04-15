262a0bc43de62be0e93f94fa829a8f242bdeec84
VINÍCIUS FERREIRA FERNANDES MARQUES ROMERO <vinicius_romero@estudante.sesisenai.org.br>
2026-04-15 12:36:09 -0300

    QÇKLEGHQPJKLEGT

--- banco.sql
+++ banco.sql
@@ -1,14 +1,16 @@
+CREATE TABLE IF NOT EXISTS responsavel (
+    id_responsavel SERIAL PRIMARY KEY,
+    nome Varchar(255) NOT NULL,
+    email Varchar(255) NOT NULL UNIQUE,
+    senha VARCHAR(12) NOT NULL
+);
+
+
 CREATE TABLE IF NOT EXISTS chamado (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     descricao text NOT NULL,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY id INTEGER NOT NULL REFERENCES responsavel(id) ON DELETE CASCADE
+    id_responsavel INTEGER NOT NULL REFERENCES responsavel(id_responsavel) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS responsavel (
-    id SERIAL PRIMARY KEY,
-    nome Varchar(255) NOT NULL,
-    email Varchar(255) NOT NULL UNIQUE,
-    senha PASSWORD NOT NULL
-);
--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-
-
-
 import Fastify from 'fastify'
 import { Pool } from 'pg'
 import cors from '@fastify/cors'
@@ -19,12 +16,13 @@ servidor.register(cors, {
     origin: '*'
 });
 
+
 servidor.post('/login', async (request, reply) => {
     const body = request.body;
-    if (!body || !body.nome || !body.email || !body.senha) {
+    if (!body || !body.nome || !body.senha || !body.email) {
         reply.status(400).send({error: "Nome, email e senha obrigatórios!"})
     }
-    const resultado = await sql.query('select * from responsavel where nome = $1, email = $2 AND senha = $3', [body.nome, body.email, body.senha])    
+    const resultado = await sql.query('select * from responsavel where nome = $1 AND senha = $2 AND email = $2' [body.nome, body.senha, body.email])    
 
     if (resultado.rows.length === 0) {
         reply.status(401).send({message: "Usuário ou senha inválidos!", login: false})
@@ -33,6 +31,7 @@ servidor.post('/login', async (request, reply) => {
     }
 
 })
+
 
 servidor.get('/responsavel', async () => {
     const resultado = await sql.query('select * from responsavel')
@@ -66,16 +65,35 @@ servidor.put('/responsavel/:id', async (request, reply) => {
         })
     }
 
-    const responsavel = await sql.query('select * from responsavel where id = $1', [id])  
+    const responsavel = await sql.query('select * from responsavel where id_responsavel = $1', [id])  
     if (responsavel.rows.length === 0) {
         return reply.status(400).send({
             message: "Usuário não existe!"
         })
     }
 
-    const resultado = await sql.query('UPDATE responsavel SET nome = $1, senha = $2, email = $4 WHERE id = $3', [body.nome, body.senha, id, body.email])      
+    const resultado = await sql.query('UPDATE responsavel SET nome = $1, senha = $2, email = $3 WHERE id_responsavel = $4', [body.nome, body.senha, body.email, id])      
     reply.status(201).send({message: `Responsável ${body.nome} alterado!`})          
 })
+
+servidor.get('/chamados', async () => {
+    const resultado = await sql.query('select * from chamado')
+    return resultado.rows
+})
+
+servidor.post('/chamados', async (request, reply) => {
+    const body = request.body;
+
+    if (!body || !body.titulo || !body.descricao) {
+        return reply.status(400).send({
+            message:"título, descrição e id do responsável obrigatórios!"
+        })
+    }
+
+    const resultado = await sql.query('INSERT INTO chamado (titulo, descricao, id_responsavel) VALUES ($1, $2, $3)', [body.titulo, body.descricao, body.id_responsavel])          
+    reply.status(201).send({message: 'Chamado Criado!'})
+})
+
 
 servidor.delete('/chamados/:id', async (request, reply) => {
     const id = request.params.id
@@ -84,6 +102,30 @@ servidor.delete('/chamados/:id', async (request, reply) => {
     reply.status(200).send({message:'Responsável Deletado!'})
 })
 
+servidor.put('/chamados/:id', async (request, reply) => {
+    const body = request.body;
+    const id = request.params.id;
+
+    if (!body || !body.titulo || !body.descricao) {
+        return reply.status(400).send({
+            message: "Titúlo, descrição do responsável são obrigatórios!"
+        })
+    } else if (!id) {
+        return reply.status(400).send({
+            message: "Faltou o ID!"
+        })
+    }
+
+    const chamado = await sql.query('select * from chamado where id = $1', [id])  
+    if (chamado.rows.length === 0) {
+        return reply.status(400).send({
+            message: "Chamado não existe!"
+        })
+    }
+
+    const resultado = await sql.query('UPDATE chamado SET titulo = $1, descricao = $2, id_responsavel = $3 WHERE id = $4', [body.titulo, body.descricao, body.id_responsavel, id])      
+    reply.status(201).send({message: `Responsável ${body.titulo} alterado!`})          
+})
 servidor.listen({   
     port: 3000
 })
